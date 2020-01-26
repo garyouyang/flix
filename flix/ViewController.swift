26362878f899df705350b3b661a9b8ff6d3d572f
garyouyang <49287999+garyouyang@users.noreply.github.com>
2020-01-25 21:25:46 -0800

    flix

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Gary Ouyang on 1/24/20.
+//  Copyright © 2020 Gary Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
